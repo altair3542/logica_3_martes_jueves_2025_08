8dff309a53af24cc1ea094b292b7139f71e1866d
altair3542 <thiagomesa21@gmail.com>
2025-07-29 18:41:09 -0500

    Agregar contenido inicial sobre estructuras de datos y arreglos en clase_1.py

---
+++ clase_1/clase_1.py
@@ -0,0 +1,13 @@
+# Estructrturas de datos y definicion de arreglos
+
+# al finalizar esta sesion, vamos a poder:
+
+#1. comprender que es una estructura de datos y por que son importantes en la programacion.
+
+#2. identificar las caracteristicas claves de los arreglos: contiguidad, accesos por incide, cuales son las ventajas de usarlos y como se limitan.
+
+# 3. diferenciar entre vectores unidimensionales y matrices multidimensionales.
+
+# 4. escribiy codigo python para crear y recorrer arreglos, manejando posibles errores en el indice
+
+# 5. resolver ejercicios practicos de definicion e inicializacion de vectores y matrices.
