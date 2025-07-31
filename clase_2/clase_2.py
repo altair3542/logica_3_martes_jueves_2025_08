a07d308f19437abb079ac80b5febf0fced8c2b4f
altair3542 <thiagomesa21@gmail.com>
2025-07-31 18:49:47 -0500

    Agregar contenido sobre arreglos unidimensionales y objetivos de aprendizaje en clase_2.py

---
+++ clase_2/clase_2.py
@@ -0,0 +1,13 @@
+#Arreglos unidimensionales (vectores)
+
+# Al finalizar esta sesión, los estudiantes serán capaces de:
+
+# Inicializar arreglos unidimensionales (vectores) de forma literal y dinámica.
+
+# Recorrer un vector utilizando for, while y funciones de orden superior como map.
+
+# Aplicar operaciones comunes: inserción, eliminación y búsqueda lineal.
+
+# Comprender la complejidad computacional básica de dichas operaciones.
+
+# Resolver problemas prácticos manipulando vectores en Python.
