1842fd43e0699f9152b58785487848a976b34480
altair3542 <thiagomesa21@gmail.com>
2025-07-31 19:08:55 -0500

    Agregar explicación sobre arreglos unidimensionales y ejemplos de inicialización en clase_2.py

--- clase_2/clase_2.py
+++ clase_2/clase_2.py
@@ -11,3 +11,25 @@
 # Comprender la complejidad computacional básica de dichas operaciones.
 
 # Resolver problemas prácticos manipulando vectores en Python.
+
+# 2.1 ¿que es un arreglo unidimensional?
+# un arreglo unidimensional es una estructura de datos que almacena una secuencia de elementos del mismo tipo logico. si bien, un arreglo puede contener todos los tipos de datos al tiempo, es una buena practica que el tipo sea igual para todos los elementos, es decir, un arreglo de arreglos, un arreglo numeros, de cadenas, de booleanos, etc.
+
+# podemos inicializar los arreglos de dos formas.
+
+# la asignacion estatica o literal:
+
+numeros = [3, 5, 7, 9]
+
+# Dinamica (por construccion en tiempo de ejecucion.)
+
+n = 5
+num = []
+
+for i in range(n):
+    valor = int(input(f"ingrese el valor {i + 1}: "))
+    num.append(valor)
+    print(num)
+
+
+
